ae6dde8b939f44f30a71241a930b180455fd7f44
DPP <dpeczek93@gmail.com>
2016-12-24 11:59:08 +0100

    Simulation on CPU

--- Zadanie4/src/main.cu
+++ Zadanie4/src/main.cu
@@ -21,37 +21,30 @@
 
 #define G (6.67408*pow(10,-11))
 
-const double min = -1000;
-const double max = 1000;
+//#define G 2
+
+const double minStartedPositionValue = -10.0;
+const double maxStartedPositionValue = 10.0;
+
+const double maxStartedWeightValue = 10000.0;
+const double maxStartedVelocityValue = 15.0;
+
+const double timeDifference = 0.3;
 
 using namespace std;
 
 
 struct position
 {
-	double x;
-	double y;
-	double z;
-	position (double x, double y, double z)
-	: x(x),
-	  y(y),
-	  z(z)
-	{
-
-	}
+	double x {};
+	double y {};
+	double z {};
 };
 
 struct physic_body {
-	position r;
-	double v;
-	double m;
-	physic_body (position r, double v, double m)
-	: r (r),
-	  v (v),
-	  m (m)
-	{
-
-	}
+	position r {};
+	double v {};
+	double m {};
 };
 
 __global__ void universe(int* N, physic_body* tab)
@@ -62,6 +55,7 @@ __global__ void universe(int* N, physic_body* tab)
 timespec diff(timespec start, timespec end);
 double computeDistance(position positionA, position positionB);
 void generateBodyValues (physic_body* bodies, int N);
+void computeOnCPU (physic_body* bodies, int N, int iterations);
 
 int main(int argc, char* argv[])
 {
@@ -74,14 +68,27 @@ int main(int argc, char* argv[])
 
 	const int N = stoi(s);
 
-	cout << G << endl;
+	physic_body cpuBodies[N];
+	physic_body gpuBodies[N];
+	generateBodyValues(cpuBodies, N);
+
+	cout << "G" << G << endl;
+
+	computeOnCPU(cpuBodies, N, iterations);
 
 	return 0;
 }
 
 void generateBodyValues (physic_body* bodies, int N)
 {
-
+	for (int i = 0; i < N; ++i)
+	{
+		bodies[i].r.x = minStartedPositionValue + (rand() % (int)(maxStartedPositionValue - minStartedPositionValue + 1));
+		bodies[i].r.y = minStartedPositionValue + (rand() % (int)(maxStartedPositionValue - minStartedPositionValue + 1));
+		bodies[i].r.z = minStartedPositionValue + (rand() % (int)(maxStartedPositionValue - minStartedPositionValue + 1));
+		bodies[i].v   = (rand() % (int)(maxStartedVelocityValue + 1));
+		bodies[i].m   = (rand() % (int)(maxStartedWeightValue + 1));
+	}
 }
 
 timespec diff(timespec start, timespec end) {
@@ -100,4 +107,36 @@ double computeDistance(position positionA, position positionB) {
 	return sqrt(pow(positionA.x - positionB.x, 2)
 			+ pow(positionA.y - positionB.y, 2)
 			+ pow(positionA.z - positionB.z, 2));
+}
+
+void computeOnCPU (physic_body* bodies, int N, int iterations) {
+	for (int iter = 0; iter < iterations; ++iter) {
+		printf ("Iteration %d \n", iter+1);
+		//Change velocity
+//		printf ("Updating velocity\n");
+		for (int i = 0; i < N; ++i) {
+			double solution = 0;
+			for (int j = 0; j < N; ++j) {
+				if (j == i) {
+					continue;
+				}
+				double dist = computeDistance(bodies[i].r, bodies[j].r);
+				solution += (bodies[j].m/(pow(dist, 3))) * dist;
+			}
+//			printf ("Body %d velocity: %lf \n", i, bodies[i].v);
+//			printf ("Body %d velocity changing by: %lf \n", i, bodies[i].v + G * solution * timeDifference);
+			bodies[i].v = bodies[i].v + G * solution * timeDifference;
+			printf ("Body %d velocity changed to %lf \n", i, bodies[i].v);
+		}
+
+		//Change position
+//		printf ("Updating position\n");
+		for (int i = 0; i < N; ++i) {
+//			printf ("Body %d position: %lf %lf %lf \n", i, bodies[i].r.x, bodies[i].r.y, bodies[i].r.z);
+			bodies[i].r.x = bodies[i].r.x + bodies[i].v * timeDifference;
+			bodies[i].r.y = bodies[i].r.y + bodies[i].v * timeDifference;
+			bodies[i].r.z = bodies[i].r.z + bodies[i].v * timeDifference;
+			printf ("Body %d position changed to: %lf %lf %lf \n", i, bodies[i].r.x, bodies[i].r.y, bodies[i].r.z);
+		}
+	}
 }
